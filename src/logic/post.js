c98db0d7c0781e567642272ab9b77d5147d01799
lscho <me@lscho.com>
2018-03-23 16:26:14 +0800

    增加评论验证，文章导航

--- src/logic/post.js
+++ src/logic/post.js
@@ -1,5 +1,33 @@
 module.exports = class extends think.Logic {
-  indexAction() {
+  commentAction() {
+    const rules = {
+      author: {
+        string: true,
+        required: true
+      },
+      email: {
+        string: true,
+        email: true
+      },
+      url: {
+        string: true,
+        url: true
+      },
+      text: {
+        string: true,
+        required: true
+      }
+    };
+    const msgs = {
+      author: '留言姓名不能为空',
+      email: '邮箱不能为空',
+      url: '地址不能为空',
+      text: '内容不能为空'
 
+    };
+    const flag = this.validate(rules, msgs);
+    if (!flag) {
+      return this.fail(1001, 'validate error', this.validateErrors);
+    }
   }
 };
