debe5c6ee3ed8008aba04c16f92c31cd5781226e
lscho <me@lscho.com>
2020-07-13 00:36:40 +0800

    修复chunk重复问题

--- src/extend/controller.js
+++ src/extend/controller.js
@@ -23,7 +23,7 @@ module.exports = {
      * @return {[type]}     [description]
      */
     async renderAndFlush(tpl) {
-        const firstChunkMinLength = 4096;
+        const firstChunkMinLength = 2014;
         let content = await this.render(tpl);
 
         //first chunk
@@ -32,12 +32,25 @@ module.exports = {
             this.ctx.flushHeaders();
 
             let length = content.length;
+
+            // 第一个 chunk 太小会被浏览器缓存起来达不到效果，参考：https://stackoverflow.com/questions/16909227/using-transfer-encoding-chunked-how-much-data-must-be-sent-before-browsers-s
             if (length < firstChunkMinLength) {
                 content += `<s>${' '.repeat(firstChunkMinLength - length)}</s>`;
             }
         }
 
         this.ctx.res.write(content);
+    },
+
+    /**
+     * 跳转页面
+     * 已经发送了正文，就不能再通过 30X 状态码和 Location 头部来跳转页面，输出一段js来跳转
+     * @param  {[type]} url [description]
+     * @return {[type]}     [description]
+     */
+    redirect(url){
+        this.ctx.res.write(`<script>window.location.href="${url}"</script>`);
+        return this.ctx.res.end();
     }
 
 };
